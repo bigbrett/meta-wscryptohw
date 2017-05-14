3dbded8f549852fcdec420dae5e6fd5c101b3964
Brett <brett.r.nicholas@gmail.com>
2017-05-14 09:18:23 -0400

    added self-chechking test code to wssha256test.c

--- recipes-kernel/wssha256test/files/wssha256test.c
+++ recipes-kernel/wssha256test/files/wssha256test.c
@@ -19,45 +19,91 @@
 #define SHA256_DGST_SIZE 32
 
 static char digest[SHA256_DGST_SIZE];     ///< The receive buffer from the LKM
-static char msg[SHA256_MSG_SIZE]; 
+static char messages[3][SHA256_MSG_SIZE];
+static char message[SHA256_MSG_SIZE];
+static const char correct[3][SHA256_DGST_SIZE] = { 
+  {0x67,0xf0,0x22,0x19,0x5e,0xe4,0x05,0x14,0x29,0x68,0xca,0x1b,0x53,0xae,0x25,0x13,0xa8,0xba,0xb0,0x40,0x4d,0x70,0x57,0x77,0x85,0x31,0x6f,0xa9,0x52,0x18,0xe8,0xba},
+  {0x6c,0x50,0x76,0x06,0x1b,0x0c,0xc3,0x1f,0x39,0x87,0x76,0x7c,0x06,0x2c,0xd1,0x33,0xab,0x13,0x07,0x34,0xa0,0xb8,0x18,0x4c,0x65,0xd0,0x65,0x88,0x18,0x23,0xb9,0x92 },
+  {0x87,0x9a,0x0a,0xa9,0xbb,0xa8,0x7c,0x38,0x8e,0x07,0x0f,0xd7,0x04,0xce,0xa2,0x50,0x20,0xa4,0x57,0x39,0x60,0x7b,0x6f,0xa0,0xbe,0x5d,0xbf,0x98,0x78,0x66,0x26,0x2c }};
 
-int main(){
+int main()
+{
+  int ret, fd;
 
+  // create the three test vectors, one with all (ASCII) zeros, one with ABCDEFGH.., 
+  // and one with ABCDEFGH... but with an ASCCI zero in the last element
+  memset((void*)messages[0],'0',SHA256_MSG_SIZE);
+  for (int i=0; i<SHA256_MSG_SIZE; i++) 
+  {
+    messages[1][i] = 'A'+(i%26);
+    messages[2][i] = messages[1][i];
+  }
+  messages[2][SHA256_MSG_SIZE-1] = '0'; 
+
+  // initialize digest to all zeros
   memset((void*)digest,0,SHA256_DGST_SIZE);
 
-  // Fill data buffer with something interesting to hash
-  printf("Data to hash:\n");
-  for (int i=0; i<SHA256_MSG_SIZE; i++) {
-    msg[i] = 'A'+(i%26);
-  }
-
-  int ret, fd;
+  // Open the device with read/write access
   printf("Starting device test code example...\n");
-  fd = open("/dev/wssha256char", O_RDWR);             // Open the device with read/write access
+  fd = open("/dev/wssha256char", O_RDWR);             
   if (fd < 0){
     perror("Failed to open the device...");
     return errno;
   }
 
-  ret = write(fd, msg, strlen(msg)); // Send the string to the LKM
-  if (ret < 0){
-    perror("Failed to write the message to the device.");
-    return errno;
+  // Run the three test cases
+  for (int testnum=0; testnum<3; testnum++)
+  {
+    // Print message to hash
+    printf("Data to hash:\n");
+    for (int i=0; i<SHA256_MSG_SIZE; i++) 
+    {
+      //if (messages[testnum][i] < 48)
+      //  printf("%u",(unsigned int)messages[testnum][i]);
+      //else
+        printf("%c",messages[testnum][i]);
+    }
+    printf("\n");
+
+    // send the test vector to LKM
+    //memcpy(message, messages[testnum],SHA256_MSG_SIZE);
+    ret = write(fd, messages[testnum], SHA256_MSG_SIZE);
+    if (ret < 0){
+      perror("Failed to write the message to the device.");
+      return errno;
+    }
+
+    // read back the response from the LKM and print
+    ret = read(fd, digest, SHA256_DGST_SIZE);        
+    if (ret < 0){
+      perror("Failed to read the message from the device.");
+      return errno;
+    }
+    printf("The received message is: ");
+    for (int i=0; i<SHA256_DGST_SIZE; i++)
+      printf("%X ", digest[i]);
+    printf("\n");
+
+    // check against known solution
+    for (int i=0; i<SHA256_DGST_SIZE; i++)
+    {
+      if (digest[i] != correct[testnum][i])
+      {
+        printf("Error, incorrect digest value for test vector %d, element %i!",testnum,i);
+        return -1;
+      }
+    }
+    printf("****Test vector %d status: SUCCESS\n",testnum);
+      
+    // reset digest to all zeros
+    memset((void*)digest,0,SHA256_DGST_SIZE);
+
+    sleep(1);
   }
 
-  ret = read(fd, digest, SHA256_DGST_SIZE);        // Read the response from the LKM
-  if (ret < 0){
-    perror("Failed to read the message from the device.");
-    return errno;
-  }
-
+  // close and exit
   if(close(fd)<0)
     perror("Error closing file");
 
-  
-  printf("*The received message is:\n");
-  for (int i=0; i<SHA256_DGST_SIZE; i++)
-    printf("%X ", digest[i]);
-  printf("\n");
   return 0;
 }
