df8c187338c958b1d019b2c067999a0ba42ec767
Brett <brett.r.nicholas@gmail.com>
2017-06-23 07:02:48 -0400

    renamed argument to aes256setiv

--- recipes-wsaescbc/wsaescbc-api/files/wsaescbc.h
+++ recipes-wsaescbc/wsaescbc-api/files/wsaescbc.h
@@ -14,5 +14,5 @@ typedef enum { RESET = 0, ENCRYPT, DECRYPT, SET_IV, SET_KEY } ciphermode_t;
 
 int32_t aes256init(void);
 int32_t aes256setkey(uint8_t *keyp);
-int32_t aes256setiv(uint8_t *keyp); 
+int32_t aes256setiv(uint8_t *ivp); 
 int32_t aes256(int mode,uint8_t *inp, uint32_t inlen,uint8_t *outp,uint32_t *outlenp);
